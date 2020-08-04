8376b586f1463c6e6f30d2824fc7a49414ca02c5
Robert Clarke <robert@robertjc.co.uk>
2020-08-04 14:13:42 +0100

    add boot sect that uses org to offset addresses

---
+++ boot_sect_memory_org.asm
@@ -0,0 +1,15 @@
+[org 0c7c00]
+mov ah, 0x0e
+
+; having solved the memory offset problem with 'org', this is now the correct answer
+mov al, [the_secret]
+int 0x10
+
+jmp $ ; infinite loop
+
+the_secret:
+	db "X"
+	
+; zero padding and magic bios number
+times 510-($-$$) db 0
+dw 0xaa55
