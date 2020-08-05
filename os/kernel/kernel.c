c755f1bbbe1355e4282f80640ba414044f58da51
Robert Clarke <robert@robertjc.co.uk>
2020-08-05 17:43:04 +0100

    another typo

--- os/kernel/kernel.c
+++ os/kernel/kernel.c
@@ -2,7 +2,7 @@
 
 void main() {
 	clear_screen();
-	krpint_at("X", 1, 6);
+	kprint_at("X", 1, 6);
 	kprint_at("This text spans multiple lines", 75, 10);
 	kprint("There is a line\nbreak");
 	kprint_at("What happens when we rund out of space?", 45, 24);
