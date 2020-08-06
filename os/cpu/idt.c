6314d0dba9d29d3c8cef7424cd3b604b0ec00c25
Robert Clarke <robert@robertjc.co.uk>
2020-08-06 12:38:41 +0100

    nope

--- os/cpu/idt.c
+++ os/cpu/idt.c
@@ -2,7 +2,6 @@
 #include "../kernel/util.h"
 
 void set_idt_gate(int n, u32 handler) {
-	extern idt;
 	idt[n].low_offset = low_16(handler);
 	idt[n].sel = KERNEL_CS;
 	idt[n].always0 = 0;
@@ -11,7 +10,6 @@ void set_idt_gate(int n, u32 handler) {
 }
 
 void set_idt() {
-	extern idt_reg;
 	idt_reg.base = (u32) &idt;
 	idt_reg.limit = IDT_ENTRIES * sizeof(idt_gate_t) - 1;
 	/* Don't make the mistake of loading &idt -- always load &idt_reg */
