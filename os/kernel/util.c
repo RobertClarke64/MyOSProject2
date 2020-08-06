7be56a0eb78065f9cbf5ff79255ce97392f937bd
Robert Clarke <robert@robertjc.co.uk>
2020-08-06 13:34:39 +0100

    a typo

--- os/kernel/util.c
+++ os/kernel/util.c
@@ -29,7 +29,7 @@ void int_to_ascii(int n, char str[]) {
 }
 
 /* K&R */
-void revers(char s[]) {
+void reverse(char s[]) {
 	int c, i, j;
 	for(i = 0, j = strlen(s)-1; i < j; i++, j--) {
 		c = s[i];
