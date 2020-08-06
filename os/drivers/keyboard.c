3389eb017475e1743fe6297605aaf0e149046da2
Robert Clarke <robert@robertjc.co.uk>
2020-08-06 16:22:01 +0100

    maybe fix backspace issue

--- os/drivers/keyboard.c
+++ os/drivers/keyboard.c
@@ -29,7 +29,7 @@ static void  keyboard_callback(registers_t regs) {
 	u8 scancode = port_byte_in(0x60);
 	
 	if(scancode > SC_MAX) return;
-	if(scancode == BACKSPACE) {
+	if(scancode == BACKSPACE && strlen(keybuffer) > 0) {
 		backspace(key_buffer);
 		kprint_backspace();
 	} else if(scancode == ENTER) {
